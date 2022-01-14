ed1af23d803a9d2e98599c1055b5ceba790d480d
contrast-zone <33665826+contrast-zone@users.noreply.github.com>
2022-01-14 12:48:33 +0100

    configurable prompt string

--- src/telnet.php
+++ src/telnet.php
@@ -2,6 +2,7 @@
     $host = urldecode($_GET['host']);
     $port = urldecode((int)$_GET['port']);
     $cmd = urldecode($_GET['cmd']);
+    $prompt = str_replace("\\x1b", "", urldecode($_GET['prompt']));
     
     $fp = fsockopen($host, $port, $errno, $errstr, 5);
 
@@ -11,13 +12,13 @@
     } else {
         stream_set_blocking($fp, false);
         
-        send($fp, $delay, "scm\n");
-        echo send($fp, $delay, "$cmd");
+        send($fp, $delay, "scm\n", $prompt);
+        echo send($fp, $delay, "$cmd", $prompt);
 
         fclose($fp);
     }
 
-    function send ($fp, $delay, $command) {
+    function send ($fp, $delay, $command, $prompt) {
         //write to socket
         if (fwrite($fp, $command) == false) {
             return "telnet command send error";
@@ -28,7 +29,7 @@
             usleep(25000);
             $response = "";
             while (($ret = fgets($fp)) != false) {
-                if (substr($ret, -25) == "[0;34mguile[1;34m> [0m") {
+                if (substr($ret, -25) == $prompt) {
                     $response = substr($response, 0, -1);
                     return $response;
                 }
