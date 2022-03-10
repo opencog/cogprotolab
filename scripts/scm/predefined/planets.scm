c054379b1613919f223beb1b943891fcc4c2d9b3
contrast-zone <33665826+contrast-zone@users.noreply.github.com>
2022-03-10 14:23:14 +0100

    Delete planets.scm

--- scripts/scm/predefined/planets.scm
+++
@@ -1,7 +0,0 @@
-;Some relationships
-(MemberLink
-  (Concept "Sun") (Concept "Has mass"))
-(MemberLink
-  (Concept "Moon") (Concept "Has mass"))
-(MemberLink
-  (Concept "Earth") (Concept "Has mass"))
