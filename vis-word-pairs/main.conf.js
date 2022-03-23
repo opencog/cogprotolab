0fd50c457a6884c7216ac8752439dc5296c85544
contrast-zone <ivan.moony@gmail.com>
2022-03-23 08:56:57 +0100

    Update

--- vis-word-pairs/main.conf.js
+++ vis-word-pairs/main.conf.js
@@ -9,8 +9,7 @@ var init = {
     "ui-scale": "100%",
     "database": {
         "top-node": "end",
-        "num-to-show": 10,
-        "query-to-show": 0,
+        "query-to-show": 1,
         "queries": [
             {
                 "description1": "query pair-stars, ranking by pair-fmi",
@@ -30,32 +29,6 @@ var init = {
                 "query": "(sim-vmi-nav 'forward (Word \"$word\"))",
                 "ranking": "(sim-vmi-nav 'edge-score (Word \"$word1\") (Word \"$word2\"))"
             }
-            /*
-            {
-                "description1": "query pair-stars, ranking by pair-fmi",
-                "description2": "forward direction",
-                "query": "(define (pair-score EDGE) (pair-freq 'pair-fmi EDGE)) " + 
-                         "(define pair-nav (make-nav pair-stars 'right-duals 'left-duals pair-score $num-to-show)) " + 
-                         "(pair-nav 'forward (Word \"$word\"))",
-                "ranking": "(pair-nav 'edge-score (Word \"$word1\") (Word \"$word2\"))",
-            },
-            {
-                "description": "query sim-stars 0, ranking by sim-fmi",
-                "description2": "forward direction",
-                "query": "(define (sim-fmi EDGE) (cog-value-ref (sim-obj 'pair-similarity EDGE) 0)) " +
-                         "(define sim-fmi-nav (make-nav sim-stars 'right-duals 'left-duals sim-fmi $num-to-show)) " +
-                         "(sim-fmi-nav 'forward (Word \"$word\"))",
-                "ranking": "(sim-fmi-nav 'edge-score (Word \"$word1\") (Word \"$word2\"))"
-            },
-            {
-                "description": "query sim-stars 1, ranking by sim-vmi",
-                "description2": "forward direction",
-                "query": "(define (sim-vmi EDGE) (cog-value-ref (sim-obj 'pair-similarity EDGE) 1)) " +
-                "(define sim-vmi-nav (make-nav sim-stars 'right-duals 'left-duals sim-vmi $num-to-show)) " +
-                "(sim-vmi-nav 'forward (Word \"$word\"))",
-                "ranking": "(sim-vmi-nav 'edge-score (Word \"$word1\") (Word \"$word2\"))"
-            }
-            */
         ]
     }
 };
