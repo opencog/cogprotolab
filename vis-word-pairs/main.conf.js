e52b15df0aaa5d0aa5cc1500ba43316ce43b3208
contrast-zone <ivan.moony@gmail.com>
2022-03-23 16:00:26 +0100

    Update

--- vis-word-pairs/main.conf.js
+++ vis-word-pairs/main.conf.js
@@ -8,22 +8,22 @@ var init = {
     "power-consumption-optimisation": "80%",
     "ui-scale": "100%",
     "database": {
-        "top-node": "end",
+        "top-node": "top",
         "queries": [
             {
-                "description1": "query pair-stars, ranking by pair-fmi",
+                "description1": "query pair-nav, ranking by pair-fmi",
                 "description2": "forward direction",
                 "query": "(pair-nav 'forward (Word \"$word\"))",
                 "ranking": "(pair-nav 'edge-score (Word \"$word1\") (Word \"$word2\"))",
             },
             {
-                "description1": "query sim-stars 0, ranking by sim-fmi",
-                "description2": "forward direction",
-                "query": "(sim-fmi-nav 'forward (Word \"$word\"))",
+                "description1": "query sim-fmi-nav, ranking by sim-fmi",
+                "description2": "backward direction",
+                "query": "(sim-fmi-nav 'backward (Word \"$word\"))",
                 "ranking": "(sim-fmi-nav 'edge-score (Word \"$word1\") (Word \"$word2\"))"
             },
             {
-                "description1": "query sim-stars 1, ranking by sim-vmi",
+                "description1": "query sim-vmi-nav, ranking by sim-vmi",
                 "description2": "forward direction",
                 "query": "(sim-vmi-nav 'forward (Word \"$word\"))",
                 "ranking": "(sim-vmi-nav 'edge-score (Word \"$word1\") (Word \"$word2\"))"
