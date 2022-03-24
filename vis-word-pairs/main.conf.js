b38913836f3738fc8b18fdd16a73e6c5dda2c94b
contrast-zone <ivan.moony@gmail.com>
2022-03-24 08:17:41 +0100

    Update

--- vis-word-pairs/main.conf.js
+++ vis-word-pairs/main.conf.js
@@ -9,7 +9,7 @@ var init = {
     "power-consumption-optimisation": "80%",
     "ui-scale": "100%",
     "database": {
-        "top-node": "top",
+        "top-node": "playing",
         "queries": [
             {
                 "description1": "query pair-nav, ranking by pair-fmi",
